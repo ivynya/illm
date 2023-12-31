4a29b03e196b556b304b23c3e277b21e8a5b2402
ivynya <ivy@ivy.direct>
2023-12-30 17:47:38 -0800

    feat: Include video stats in summary response

--- client/summarize.go
+++ client/summarize.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gorilla/websocket"
 	"github.com/ivynya/illm/internal"
 	"github.com/kkdai/youtube/v2"
@@ -19,6 +21,12 @@ func summarize(c *websocket.Conn, req *internal.Request) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+
+	resp, err := encodeRequest(req.Tag, "response", "Video: "+video.Title+"\nTranscript length: "+strconv.Itoa(len(transcript.String()))+"\n\n")
+	if err != nil {
+		return false, err
+	}
+	c.WriteMessage(websocket.TextMessage, resp)
 
 	req.Generate.Prompt = "Summarize the following video: " + video.Title + "\n\n" + transcript.String() + "\n\nSummary:"
 	req.Generate.Context = []int{}
